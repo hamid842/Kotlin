e90aaac6078e376866940421260438b4bdebca23
hamid842 <Somayeh@842>
2019-07-18 14:45:59 +0430

    do new tests

---
+++ src/Maximum.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val a: Int = 26
+    val b: Int = 9879
+    val max: Int
+    if (a > b) {
+        max = a
+    } else {
+        max = b
+    }
+    println("Maximum of (a) or (b) is:" + max)
+}
---
+++ src/UseOfWhen.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val day:Int = 4
+    when(day){                       //Like "switch" in JAVA
+        1 -> println("Saturday")
+        2 -> println("Monday")
+        3 -> println ("Sunday")
+        4 -> println("Tuseday")
+        5 -> println("Wednsday")
+        6 -> println("Thursday")
+        7 -> println("Friday")
+        else ->{
+            println("looking forward to the weekday")
+        }
+    }
+}
