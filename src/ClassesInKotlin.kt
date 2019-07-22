e38c11bd0e5cd21016d6b5c02d0fcc98ba091c63
hamid842 <Somayeh@842>
2019-07-22 20:15:56 +0430

    studying extension

---
+++ src/ClassesInKotlin.kt
@@ -0,0 +1,10 @@
+class ClassesInKotlin {
+    private var name : String = " Tarlan Mohamadi"
+    fun prinMe(){
+        print("Hamid mohamadi")
+    }
+}
+fun main(args:Array<String>){
+    val obj = ClassesInKotlin()
+    obj.prinMe()
+}
